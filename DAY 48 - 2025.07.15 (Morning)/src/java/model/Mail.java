1db2162c205a4ca0a26f5d52a9feefdf884ebddd
Jude <jmalshanmel@gmail.com>
2025-07-18 08:39:18 +0530

    back commit full again

---
+++ DAY 48 - 2025.07.15 (Morning)/src/java/model/Mail.java
@@ -0,0 +1,55 @@
+
+package model;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author tharaka@javainstitute.edu.lk
+ */
+public class Mail {
+
+    private static final String APP_EMAIL = "YOUR EMAIL";
+    private static final String APP_PASSWORD = "YOUR EMAIL APP PASSWORD";
+
+    public static void sendMail(String email, String subject, String htmlContent) {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");   // gmail use  (any other change to hotmail, outlook, etc.)
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(Mail.APP_EMAIL, Mail.APP_PASSWORD);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(Mail.APP_EMAIL));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject(subject);
+            message.setContent(htmlContent, "text/html");
+
+            Transport.send(message);
+
+            System.out.println("Email sent successfully!");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        
+    }
+    
+}
